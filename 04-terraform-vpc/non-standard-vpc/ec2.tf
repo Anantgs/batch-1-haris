4a3a79be78f2409fa9d62f3c43567e3aad2fd57c
harishazmohd <haresmohd012@gmail.com>
2026-02-14 21:21:27 +0530

    add loadbalancer and targetgroup

--- 04-terraform-vpc/non-standard-vpc/ec2.tf
+++ 04-terraform-vpc/non-standard-vpc/ec2.tf
@@ -6,28 +6,16 @@ variable "instance_map" {
   }
 }
 
-variable "aws_azs" {
-  type = list(string)
-  default = ["us-east-1a", "us-east-1b"]
-}
-
-
-
 resource "aws_instance" "app_server" {
   ami                         = data.aws_ami.example.id
   instance_type               = "t3.micro"
   vpc_security_group_ids      = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
   user_data                   = file("apache-install.sh")
-  for_each                    = zipmap(var.aws_azs, module.vpc.public_subnets)
+  for_each                    = zipmap(data.aws_availability_zones.example.names, module.vpc.public_subnets)
   subnet_id                   = each.value
   associate_public_ip_address = true
 
   tags = {
     Name = "learn-terraform-${each.key}"
   }
-}
-
-output "instance_id" {
-  description = "We will create instance id map here"
-  value       = { for key, instance in aws_instance.app_server : key => instance.id }
 }
